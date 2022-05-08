f10989deb38135c4c2853d08023daa1339c0025a
lawrencelee <lawrencelee0205@gmail.com>
2022-05-08 16:46:32 +0800

    DLM-0001 Initialize project.

---
+++ app/models.py
@@ -0,0 +1,45 @@
+import random
+from django.db import models
+from django_countries.fields import CountryField
+
+def generate_isbn():
+    return "".join([str(random.randint(0,9)) for _ in range(10)])
+
+BOOK_CATEGORY = (
+    ('SC', 'SCIENCTIFIC'),
+    ('AD', 'ADVENTURE'),
+    ('FC', 'FICTION'),
+    ('NFC', 'NON-FICTION'),
+)
+
+GENDER_CATEGORY = (
+    ('M','MALE'),
+    ('F','FEMALE'),
+)
+
+# Create your models here.
+class Book(models.Model):
+    isbn = models.CharField(max_length=10,unique=True,default=generate_isbn())
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=10,choices=BOOK_CATEGORY)
+    author = models.ForeignKey('Author',on_delete=models.CASCADE)
+    library = models.ManyToManyField('Library',related_name='books')
+    Publications = models.ForeignKey('Publication',on_delete=models.CASCADE,related_name='books')
+    pass
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=10,choices=GENDER_CATEGORY)
+    nationality = CountryField(blank_label='(Select country)')
+    born_date = models.DateField()
+    publication = models.OneToOneField('Publication',on_delete=models.CASCADE)
+    pass
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    district = models.CharField(max_length=200)
+    pass
+
+class Publication(models.Model):
+    name = models.CharField(max_length=200)    
+    pass
